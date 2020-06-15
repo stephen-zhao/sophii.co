d0316e0e9b89f10859b57de7448eb0b528eaa59d
Stephen Zhao <mail@zhaostephen.com>
2020-06-15 19:36:30 -0400

    Add support to choose displacement method

--- src/scripts/avoidance.js
+++ src/scripts/avoidance.js
@@ -38,8 +38,13 @@ Avoidance.getCentre = function($element, $relativeTo) {
 //   return $element.offset().top - ($relativeTo ? $relativeTo.offset().top : 0);
 // }
 
-Avoidance.getDistance = function(pointA, pointB) {
-  return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
+Avoidance.geometry = {
+  getDistance: function(pointA, pointB) {
+    return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
+  },
+  getRadius: function(point) {
+    return Math.sqrt(point.x*point.x + point.y*point.y);
+  },
 }
 
 Avoidance.calculateAvoidanceFactor = function(originalDistance, elementSize, containerSize, method) {
@@ -52,24 +57,95 @@ Avoidance.calculateAvoidanceFactor = function(originalDistance, elementSize, con
     return Avoidance.calculateAvoidanceFactor.builtinMethods[method]()(originalDistance, elementSize, containerSize);
   }
   else {
-    return Avoidance.calculateAvoidanceFactor.builtinMethods.inverse()(originalDistance, elementSize, containerSize);
+    return Avoidance.calculateAvoidanceFactor.builtinMethods.power_inverse()(originalDistance, elementSize, containerSize);
   }
 }
 
 Avoidance.calculateAvoidanceFactor.builtinMethods = {
-  "inverse": function(param_scale=0.1, param_offset=1.0) {
+  "inverse": function(param_scale=0.1, param_offset=0.0) {
     return function(originalDistance, elementSize, containerSize) {
       if (originalDistance === 0) {
         return NaN;
       }
       else {
-        return ((containerSize.width*param_scale)/originalDistance + param_offset);
+        return (containerSize.width*param_scale*1.0)/originalDistance + param_offset;
       }
     }
   },
   "exponential": function(param_scale=0.01, param_offset=0.25) {
     return function(originalDistance, elementSize, containerSize) {
-      return Math.exp(param_scale-originalDistance/containerSize.width) + param_offset;
+      return Math.exp(param_scale-originalDistance*1.0/containerSize.width) + param_offset;
+    }
+  },
+  "power_inverse": function(param_scale=1.0, param_offset=0.0, param_power=1.6) {
+    return function(originalDistance, elementSize, containerSize) {
+      if (originalDistance === 0) {
+        return NaN;
+      }
+      else {
+        return ((containerSize.width*param_scale*1.0)/Math.pow(originalDistance*1.0, param_power) + param_offset);
+      }
+    }
+  }
+}
+
+Avoidance.calculateAvoidanceDisplacement = function(childPosRelToMousePos, avoidanceFactor, method) {
+  // TODO: take into account the element size (and therefore centre)
+  if (typeof method === "function") {
+    return method(childPosRelToMousePos, avoidanceFactor)
+  }
+  else if (typeof method === "string" && method in Avoidance.calculateAvoidanceDisplacement.builtinMethods) {
+
+    return Avoidance.calculateAvoidanceDisplacement.builtinMethods[method]()(childPosRelToMousePos, avoidanceFactor);
+  }
+  else {
+    return Avoidance.calculateAvoidanceDisplacement.builtinMethods.threshold_proportional_radius()(childPosRelToMousePos, avoidanceFactor);
+  }
+}
+
+Avoidance.calculateAvoidanceDisplacement.builtinMethods = {
+  "threshold_absolute_radius": function(param_threshold_radius=100.0) {
+    return function(childPosRelToMousePos, avoidanceFactor) {
+      var offset = {
+        x: childPosRelToMousePos.x*avoidanceFactor,
+        y: childPosRelToMousePos.y*avoidanceFactor,
+      };
+      if (Avoidance.geometry.getRadius(offset) > param_threshold_radius) {
+        var origRadius = Avoidance.geometry.getRadius(childPosRelToMousePos);
+        var scaleFactor = (param_threshold_radius / origRadius);
+        return {
+          x: childPosRelToMousePos.x*scaleFactor,
+          y: childPosRelToMousePos.y*scaleFactor,
+        };
+      }
+      else {
+        return offset;
+      }
+    }
+  },
+  "threshold_proportional_radius": function(param_threshold_radius=20.0) {
+    return function(childPosRelToMousePos, avoidanceFactor) {
+      var offset = {
+        x: childPosRelToMousePos.x*avoidanceFactor,
+        y: childPosRelToMousePos.y*avoidanceFactor,
+      };
+      if (avoidanceFactor > param_threshold_radius) {
+        return {
+          x: childPosRelToMousePos.x*param_threshold_radius,
+          y: childPosRelToMousePos.y*param_threshold_radius,
+        };
+      }
+      else {
+        return offset;
+      }
+    }
+  },
+  "standard": function() {
+    return function(childPosRelToMousePos, avoidanceFactor) {
+      return {
+        x: childPosRelToMousePos.x*avoidanceFactor,
+        y: childPosRelToMousePos.y*avoidanceFactor,
+      };
     }
   }
 }
@@ -87,15 +163,20 @@ Avoidance.mouseMoveHandler = function(event) {
       y: parseInt(this.dataset.zhaostephenAvoidanceOrigy),
     };
     var childSize = { width: this.offsetWidth, height: this.offsetHeight };
-    var origDist = Avoidance.getDistance(mousePos, childPos);
-    var scaleFactor = Avoidance.calculateAvoidanceFactor(origDist, childSize, containerSize);
-    if (scaleFactor === NaN) {
+    var origDist = Avoidance.geometry.getDistance(mousePos, childPos);
+    var avoidanceFactor = Avoidance.calculateAvoidanceFactor(origDist, childSize, containerSize);
+    if (avoidanceFactor === NaN) {
       this.style.display = "none";
     }
     else {
+      var childRelPos = {
+        x: childPos.x - mousePos.x,
+        y: childPos.y - mousePos.y,
+      };
+      var avoidanceDisplacement = Avoidance.calculateAvoidanceDisplacement(childRelPos, avoidanceFactor);
       var newChildPos = {
-        x: mousePos.x + (childPos.x - mousePos.x)*scaleFactor,
-        y: mousePos.y + (childPos.y - mousePos.y)*scaleFactor,
+        x: childPos.x + avoidanceDisplacement.x,
+        y: childPos.y + avoidanceDisplacement.y,
       };
       if (this.style.display === "none") {
         this.style.display = "";
